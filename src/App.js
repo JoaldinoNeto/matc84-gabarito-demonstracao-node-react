6a3360d7c067ce5e1c0aa5122721118ae782efca
Joaldino Neto <joaldinoneto@gmail.com>
2019-10-17 12:10:25 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
+
 
 function App() {
+  const [list, setList] = useState([])
+
+  useEffect(() => {
+    const handleRequest = async () => {
+      const res = await axios.get('http://localhost:3003/list')
+
+      setList(res.data)
+    }
+
+    handleRequest()
+  }, [])
+
+  console.log(list)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        {
+          list.map((item, index) => <li key={index}>{item}</li>)
+        }
+      </ul>
     </div>
   );
 }
